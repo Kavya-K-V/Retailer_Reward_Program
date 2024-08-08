dfdb3680ecbfaa4fd2a151fdada1686e6563d438
Kavya K.V <kavya.kv01@ad.infosys.com>
2024-08-08 19:54:35 +0530

    commiting first version of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+
+const columns = [
+  {
+    name: "Customer",
+    selector: (row) => row.customer,
+  },
+  {
+    name: "Month",
+    selector: (row) => row.month,
+  },
+  {
+    name: "Amount($)",
+    selector: (row) => row.amount,
+  },
+  {
+    name: "Reward Point/Transaction",
+    selector: (row) => row.reward,
+  },
+];
+
+const newColumns = [
+  {
+    name: "Month",
+    selector: (row) => row.month,
+  },
+  {
+    name: "Reward Points/Month",
+    selector: (row) => row.totReward,
+  },
+];
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [displayReward, setDisplayReward] = useState(false);
+  const [selectedRow, setSelectedRow] = useState([]);
+  const [rewardData, setRewardData] = useState([]);
+  const [totalReward, setTotalReward] = useState(0);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/c/a874-b912-4617-a7a6")
+      .then((res) => res.json())
+      .then((resp) => {
+        //I have added a fake api with the desired json response.Please refer to SampleResponse.json file to view the response.
+        let rewardArr = [...resp];
+        rewardArr.forEach((item, i) => {
+          if (item.amount >= 100) {
+            rewardArr[i].reward = 2 * (item.amount - 100) + 1 * 50;
+          } else {
+            rewardArr[i].reward = 0;
+          }
+        });
+        setUserData(rewardArr);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleRowDoubleClick = (rowData) => {
+    let newData = [];
+    setSelectedRow(rowData);
+    setDisplayReward(true);
+    let filteredArr = userData.filter(
+      (item) => item.customer === rowData.customer
+    );
+    filteredArr.forEach((elem, i) => {
+      const index = newData.findIndex((x) => x.month === elem.month);
+      if (index !== -1) {
+        newData[index].totReward = newData[index].totReward + elem.reward;
+      } else {
+        newData.push({ month: elem.month, totReward: elem.reward });
+      }
+    });
+    setTotalReward(newData.reduce((acc, o) => acc + parseInt(o.totReward), 0));
+    setRewardData(newData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Customer Transaction Data </h1>
+      <p>Double click on a row to view the total reward points at the bottom!</p>
+      <DataTable
+        columns={columns}
+        data={userData}
+        onRowDoubleClicked={(rowData) => handleRowDoubleClick(rowData)}
+        pagination
+        striped
+      />
+      {displayReward && (
+        <>
+          <h1>Reward Points Per Month for {selectedRow.customer}</h1>
+          <DataTable columns={newColumns} data={rewardData} striped />
+          <h1>Total Reward Points:{totalReward}</h1>
+        </>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  test("renders users when API call succeeds", async () => {
+    const fakeUsers = [
+      { customer: "Kevin", month: "January", amount: 120 },
+      { customer: "Kevin", month: "February", amount: 100 },
+      { customer: "Kiran", month: "January", amount: 180 },
+      { customer: "Kiran", month: "February", amount: 200 },
+      { customer: "John", month: "January", amount: 50 },
+      { customer: "John", month: "February", amount: 100 },
+      { customer: "John", month: "March", amount: 300 },
+    ];
+
+    jest.spyOn(global, "fetch").mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(fakeUsers),
+      })
+    );
+
+    render(<App />);
+    await screen.findAllByText("Kevin");
+  });
 });
